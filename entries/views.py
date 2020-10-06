98d3ba0376a120fc69a0da5660a53e8f3407005b
melzreal <melissa.real@hotmail.com>
2020-10-06 17:00:53 +0100

    loop through db entries

--- entries/views.py
+++ entries/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Entry
 
 # the view handles what happens after typing an address
 
 def index(request):
-  return render(request, 'entries/index.html')
+  entries = Entry.objects.all()
+  context = {'entries' : entries}
+
+  return render(request, 'entries/index.html', context)
 
 def add(request):
+
   return render(request, 'entries/add.html')
