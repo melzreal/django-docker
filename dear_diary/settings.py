cd6f8c5efdf2d221fbc6d7b4262f90a8ba098e92
melzreal <melissa.real@hotmail.com>
2020-10-06 17:41:47 +0100

    notes app

--- dear_diary/settings.py
+++ dear_diary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_@(1bm7i-j3944dccmwe0rah8lcv(6=#o=t)h6rnn43%&-@_jl'
+SECRET_KEY = "${DOCKER_SECRET}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
